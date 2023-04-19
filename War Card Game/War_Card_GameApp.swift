e39b4622427e559cb283ac109fc108963b7f5356
AndreyZheleznyak <zheleznyak.dev@gmail.com>
2023-04-19 14:09:57 +0300

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by admin on 19.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
