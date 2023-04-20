7f0ea12d3fcd480969dbf43e5e3222c12c472b9a
AndreyZheleznyak <zheleznyak.dev@gmail.com>
2023-04-20 12:59:41 +0300

    feat: add button with corresponding logic

--- War Card Game/ContentView.swift
+++ War Card Game/ContentView.swift
@@ -8,27 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var playerCard = "card7"
+    @State var cpuCard = "card13"
+    
+    @State var playerScore = 0
+    @State var cpuScope = 0
+    
     var body: some View {
+        
         ZStack {
             Image("background-plain")
                 .resizable()
                 .ignoresSafeArea()
-
+            
             VStack {
                 Spacer()
                 Image("logo")
                 Spacer()
                 HStack {
                     Spacer()
-                    Image("card2")
+                    Image(playerCard)
                     Spacer()
-                    Image("card3")
+                    Image(cpuCard)
                     Spacer()
                 }
                 Spacer()
-                Button(action: {}) {
+                Button {
+                    deal()
+                } label: {
                     Image("button")
                 }
+                
                 Spacer()
                 HStack {
                     Spacer()
@@ -36,7 +47,7 @@ struct ContentView: View {
                         Text("Player")
                             .font(.headline)
                             .padding(.bottom, 10.0)
-                        Text("0")
+                        Text(String(playerScore))
                             .font(.largeTitle)
                     }
                     Spacer()
@@ -44,7 +55,7 @@ struct ContentView: View {
                         Text("CPU")
                             .font(.headline)
                             .padding(.bottom, 10.0)
-                        Text("0")
+                        Text(String(cpuScope))
                             .font(.largeTitle)
                     }
                     Spacer()
@@ -52,6 +63,24 @@ struct ContentView: View {
                 .foregroundColor(.white)
                 Spacer()
             }
+        }
+    }
+    
+    func deal() {
+        // Randomize the players card
+        var playerCardValue = Int.random(in: 2...14)
+        playerCard = "card" + String(playerCardValue)
+        
+        // Randomize the cpus card
+        var cpuCardValue = Int.random(in: 2...14)
+        cpuCard = "card" + String(cpuCardValue)
+        
+        // Update the scores
+        if playerCardValue > cpuCardValue {
+            playerScore += 1
+        }
+        else if cpuCardValue > playerCardValue {
+            cpuScope += 1
         }
     }
 }
